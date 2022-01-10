9cd02c80234a990f626f78e1c8a455c518d7aaed
cdtest <corydavis1989@gmail.com>
2022-01-10 16:40:05 -0600

    Corrected Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,71 @@
 // listen for submit
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function (e) {
+  // hide results
+  document.getElementById("results").style.display = "none";
+  // show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 function calculateResults(e) {
-// UI Vars
+  // UI Vars
 
-const amount = document.getElementById('amount');
-const interest = document.getElementById('interest');
-const years = document.getElementById('years');
-const monthlyPayment = document.getElementById('monthly-payment');
-const totalPayment = document.getElementById('total-payment');
-const totalInterest = document.getElementById('total-interest');
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
 
-const principal = parseFloat(amount.value);
-const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-const calculatedPayments = parseFloat(years.value) * 12;
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
 
-const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-const monthly = (principal*x*calculatedInterest)/(x-1);
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
 
-if(isFinite(monthly)) {
+  if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
 
-}else {
-    showError('Please check your numbers');
+    document.getElementById("results").style.display = "block";
 
-}
-    e.preventDefault();
+    document.getElementById("loading").style.display = "none";
+  } else {
+    showError("Please check your numbers");
+  }
+
 }
 
 function showError(error) {
-    const errorDiv = document.createElement('div');
+  document.getElementById("results").style.display = "none";
 
-    const card = document.querySelector('.card');
-    const heading = document.querySelector('.heading');
+  document.getElementById("loading").style.display = "none";
 
-    // add class
-    errorDiv.className = 'alert alert-danger';
+  const errorDiv = document.createElement("div");
 
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
 
-    errorDiv.appendChild(document.createTextNode(error));
+  // add class
+  errorDiv.className = "alert alert-danger";
 
-    card.insertBefore(errorDiv, heading);
+  errorDiv.appendChild(document.createTextNode(error));
 
-    // clear error after three seconds
+  card.insertBefore(errorDiv, heading);
 
-    setTimeout(clearError, 3000);
+  // clear error after three seconds
+
+  setTimeout(clearError, 3000);
 }
 
 // clear error
 
-function clearError(){
-    document.querySelector('.alert').remove();
+function clearError() {
+  document.querySelector(".alert").remove();
 }
-
