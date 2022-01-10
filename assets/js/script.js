9fba20fadde25a077badc830817c75f477ac7a0d
cdtest <corydavis1989@gmail.com>
2022-01-10 15:59:20 -0600

    added initial html and event listener

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// listen for submit
+
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e) {
+console.log('Calculating....')
+
+    e.preventDefault();
+}
